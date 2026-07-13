2319d9fc5a0cd3c3fc32a7340df4ddb8555147ab
Yuvraj Singh Tomar <yuvrajst825@gmail.com>
2026-07-13 10:34:33 +0530

    updated

--- project/student_api/database.py
+++ project/student_api/database.py
@@ -1,4 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+DATABASE_URL = "sqlite:///students.db"
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+class Base(DeclarativeBase):
+    pass
+
+
+
 try:
+
+
     from .models import Student
 except ImportError:
     from models import Student
